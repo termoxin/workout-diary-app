4cf14dceddf7b91ef83f2c70ab7525faa2eacb4a
Rostislav <rostislav.futornoy@gmail.com>
2019-11-23 12:09:21 +0200

    Feature: show progress inside of workout item

--- src/components/workoutList/WorkoutItem.jsx
+++ src/components/workoutList/WorkoutItem.jsx
@@ -2,13 +2,22 @@ import React from "react";
 
 import "../../styles/workoutItem.css";
 
-const WorkoutItem = ({ name, reps, id, date, onDelete }) => (
-  <li onClick={() => onDelete(id)}>
+const WorkoutItem = ({ name, reps, id, date, progress, onDelete }) => (
+  <li>
     <div className="workout-item">
       <span className="workout-item__name">{name}</span>
-      <span className="workout-item__count">{reps} reps</span>
+      <span
+        className={`workout-item__count ${
+          progress ? (progress > 0 && "up") || "down" : ""
+        }`}
+      >
+        {reps} reps
+        {progress ? (progress > 0 && ` ${progress}%↑`) || ` ${progress}%↓` : ""}
+      </span>
       <span className="workout-item__date">{date}</span>
-      <span className="cross">X</span>
+      <span className="cross" onClick={() => onDelete(id)}>
+        X
+      </span>
     </div>
   </li>
 );
